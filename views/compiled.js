71b294051873216aeb7b61fea024941b5542eba2
Jackson Palmer <hello@ummjackson.com>
2016-07-12 18:34:50 -0700

    Updates

--- views/compiled.js
+++ views/compiled.js
@@ -5,7 +5,7 @@ var colno = null;
 var output = "";
 try {
 var parentTemplate = null;
-output += "<div class=\"row\">\n\n    <div class=\"col-md-12 main\">\n    \n        <h4 class=\"page-header\">Table Browser</h4>\n\n        <p>Coming soon!</p>\n\n    </div>\n\n</div>\n\n<script>\n</script>";
+output += "<div class=\"row\">\n\n    <div class=\"col-md-12 main\">\n    \n        <h4 class=\"page-header\">Table Browser</h4>\n\n        <div class=\"table-responsive\" id=\"browser\">\n\n        </div>\n\n    </div>\n\n    <!-- Modal box for table details -->\n\t<div class=\"modal\" id=\"table-detail\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"table-detail\">\n\t  <div class=\"modal-dialog modal-lg\" role=\"document\">\n\t    <div class=\"modal-content\">\n\t      <div class=\"modal-header\">\n\t        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>\n\t        <h4 class=\"modal-title\" id=\"table-detail-title\">table</h4>\n\t      </div>\n\t      <div class=\"modal-body\" id=\"table-detail-body\">\n\t\t      <h5>Columns</h6>\n\t\t      <ul id=\"table-columns\"></ul>\n\t\t      <h5>Sample</h6>\n\t\t      <div class=\"table-responsive\" id=\"browser-sample\">\n\t    \t\t\t<table class=\"table table-striped table-sm\" id=\"table-sample\"></table>\n\t\t\t  </div>\n\t      </div>\n\t      <div class=\"modal-footer\">\n\t        <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>\n\t      </div>\n\t    </div>\n\t  </div>\n\t</div>\n\n</div>\n\n<script>\n\n$('#browser').html(nunjucks.render('partials/table-browser.html', {tables: alasql('SHOW TABLES;')}));\n\n$(\".table-name\").click(function(event) {\n\tevent.preventDefault();\n\n\t// Get table ID\n\ttableid = $(this).data(\"tableid\");\n\n\t// Set title of modal\n\t$('#table-detail-title').text(\"Table: \" + tableid);\n\n\t// Render sample\n    alasql.promise('SELECT * FROM ' + tableid + ' LIMIT 10;')\n    .then(function(res){\n        alasql('SELECT * INTO HTML(\"#table-sample\", {headers:true}) FROM ?',[res]);\n\n\t\t// Get & set columns\n\t\tcolumns = [];\n\t\t$('#table-columns').empty();\n\t\t$('#table-sample thead th').each(function() {\n\t\t    $('#table-columns').append('<li>' + $(this).text() + '</li>');\n\t\t});\n\n\t\t// Pop modal\n\t\t$('#table-detail').modal('show');\n\n    }).catch(function(err){\n        console.log('There was an error:', err);\n    });\n\n\t});\n\n</script>";
 if(parentTemplate) {
 parentTemplate.rootRenderFunc(env, context, frame, runtime, cb);
 } else {
@@ -112,6 +112,55 @@ output += "</td>\n                    </tr>\n                    ";
 }
 frame = frame.pop();
 output += "\n            </table>\n                </tbody>";
+if(parentTemplate) {
+parentTemplate.rootRenderFunc(env, context, frame, runtime, cb);
+} else {
+cb(null, output);
+}
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+root: root
+};
+
+})();
+})();
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["partials/table-browser.html"] = (function() {
+function root(env, context, frame, runtime, cb) {
+var lineno = null;
+var colno = null;
+var output = "";
+try {
+var parentTemplate = null;
+output += "    <table class=\"table table-striped table-sm\">\n        <thead>\n            <tr>\n                <th>Table</th>\n                <th>Actions</th>\n            </tr>\n        </thead>\n        <tbody>\n            ";
+frame = frame.push();
+var t_3 = env.getFilter("sort").call(context, runtime.contextOrFrameLookup(context, frame, "tables"),true,false,"tableid");
+if(t_3) {var t_2 = t_3.length;
+for(var t_1=0; t_1 < t_3.length; t_1++) {
+var t_4 = t_3[t_1];
+frame.set("table", t_4);
+frame.set("loop.index", t_1 + 1);
+frame.set("loop.index0", t_1);
+frame.set("loop.revindex", t_2 - t_1);
+frame.set("loop.revindex0", t_2 - t_1 - 1);
+frame.set("loop.first", t_1 === 0);
+frame.set("loop.last", t_1 === t_2 - 1);
+frame.set("loop.length", t_2);
+output += "\n            <tr>\n                <td><a href=\"#browser\" class=\"table-name\" data-tableid=\"";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"tableid"), env.opts.autoescape);
+output += "\">";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"tableid"), env.opts.autoescape);
+output += "</a></td>\n                <td><a href=\"#browser\" class=\"text-danger\" data-tableid=\"";
+output += runtime.suppressValue(runtime.memberLookup((t_4),"tableid"), env.opts.autoescape);
+output += "\">DROP</a></td>\n            </tr>\n            ";
+;
+}
+}
+frame = frame.pop();
+output += "\n    </table>\n        </tbody>";
 if(parentTemplate) {
 parentTemplate.rootRenderFunc(env, context, frame, runtime, cb);
 } else {
